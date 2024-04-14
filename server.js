d0899b545716a20000f9ddf863f7e8527d87d3f0
MChandraR <2201020103@student.umrah.ac.id>
2024-04-14 23:10:14 +0700

    Code updated

--- server.js
+++ server.js
@@ -7,13 +7,46 @@ const httpServer = createServer(server);
 server.get("/",(req,res)=>{
     res.send("hallo");
 })
+httpServer.listen(3000);
+server.use(express.json());
+server.use(express.static('public'));
+
+let data = {};
 
 server.post("/test",(req,res)=>{
     console.log(req.body);
+    let body = req.body;
+    if(body.id!=null && body.id!=""){
+        data[body.id] = {
+            "location" : body.location,
+            "temp" : body.temp,
+            "hum" : body.hum
+        };
+    }
+  
+    console.log(data);
     res.send({
         "status" : "berhasil",
         "data" : req.body
     });
 });
 
-httpServer.listen(3000);
+const fs = require('fs');
+const inputFile ='./view.html' ;
+
+    
+server.get("/data",(req,res)=>{
+    fs.readFile(inputFile, 'utf8', async (err, data) => {
+        if(err){
+            res.send("Error");
+            return;
+        }
+        res.send(data);
+    });
+    
+});
+
+server.get("/getdata",(req,res)=>{
+    res.send(data);
+});
+
